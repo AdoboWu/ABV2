7d38d503265e6ac7dafabd79ffb6d42c74d992be
かいと <160225758+AdoboWu@users.noreply.github.com>
2024-03-03 00:30:05 +0800

    Update pending.js

--- script/pending.js
+++ script/pending.js
@@ -1,84 +1,140 @@
 module.exports.config = {
-	name: "pending",
-	version: "1.0.5",
-	credits: "Mirai Team",
-	hasPermssion: 2,
-	usePrefix: true,
-	description: "Manage bot's waiting messages",
-	commandCategory: "system",
-	cooldowns: 5
+    name: "pending",
+    version: "1.0.6",
+    credits: "CatalizCS mod by Kadeer",
+    hasPermssion: 2,
+    description: "Quản lý tin nhắn chờ của bot",
+    usePrefix: false,
+    commandCategory: "Admin",
+    usages: "[u] [t] [a]",
+    cooldowns: 0
 };
 
-module.exports.languages = {
-		/*"vi": {
-				"invaildNumber": "%1 không phải là một con số hợp lệ",
-				"cancelSuccess": "Đã từ chối thành công %1 nhóm!",
-				"notiBox": "Box của bạn đã được admin phê duyệt để có thể sử dụng bot",
-				"approveSuccess": "Đã phê duyệt thành công %1 nhóm!",
 
-				"cantGetPendingList": "Không thể lấy danh sách các nhóm đang chờ!",
-				"returnListPending": "「PENDING」❮ Tổng số nhóm cần duyệt: %1 nhóm ❯\n\n%2",
-				"returnListClean": "「PENDING」Hiện tại không có nhóm nào trong hàng chờ"
-		},*/
-		"en": {
-				"invaildNumber": "%1 is not an invalid number",
-				"cancelSuccess": "Refused %1 thread!",
-				//"notiBox": "🌐 BoT Connected Successfully✅!\nUse /help for more info :>",
-				"approveSuccess": "Approved successfully %1 threads!",
-
-				"cantGetPendingList": "Can't get the pending list!",
-				"returnListPending": "»「PENDING」«❮ The whole number of threads to approve is: %1 thread ❯\n\n%2",
-				"returnListClean": "「PENDING」There is no thread in the pending list"
-		}
+module.exports.onLoad = () => {
+    const fs = require("fs-extra");
+    const request = require("request");
+    const dirMaterial = __dirname + `/trogiup/menu/`;
+    if (!fs.existsSync(dirMaterial + "menu")) fs.mkdirSync(dirMaterial, { recursive: true });
+    if (!fs.existsSync(dirMaterial + "trogiup.jpg")) request("https://i.imgur.com/65WOdNC.png").pipe(fs.createWriteStream(dirMaterial + "trogiup.png"));
 }
 
 module.exports.handleReply = async function({ api, event, handleReply, getText }) {
-		if (String(event.senderID) !== String(handleReply.author)) return;
-		const { body, threadID, messageID } = event;
-		var count = 0;
+    if (String(event.senderID) !== String(handleReply.author)) return;
+    const { body, threadID, messageID } = event;
+    var count = 0;
 
-		if (isNaN(body) && body.indexOf("c") == 0 || body.indexOf("cancel") == 0) {
-				const index = (body.slice(1, body.length)).split(/\s+/);
-				for (const singleIndex of index) {
-						console.log(singleIndex);
-						if (isNaN(singleIndex) || singleIndex <= 0 || singleIndex > handleReply.pending.length) return api.sendMessage(getText("invaildNumber", singleIndex), threadID, messageID);
-						api.removeUserFromGroup(api.getCurrentUserID(), handleReply.pending[singleIndex - 1].threadID);
-						count+=1;
-				}
-				return api.sendMessage(getText("cancelSuccess", count), threadID, messageID);
-		}
-		else {
-				const index = body.split(/\s+/);
-				for (const singleIndex of index) {
-						if (isNaN(singleIndex) || singleIndex <= 0 || singleIndex > handleReply.pending.length) return api.sendMessage(getText("invaildNumber", singleIndex), threadID, messageID);
-						api.sendMessage(getText("notiBox"), handleReply.pending[singleIndex - 1].threadID);
-						count+=1;
-				}
-				return api.sendMessage(getText("approveSuccess", count), threadID, messageID);
-		}
+    if (isNaN(body) && body.indexOf("c") == 0 || body.indexOf("cancel") == 0) {
+        const index = (body.slice(1, body.length)).split(/\s+/);
+        for (const singleIndex of index) {
+            console.log(singleIndex);
+            if (isNaN(singleIndex) || singleIndex <= 0 || singleIndex > handleReply.pending.length) return api.sendMessage(`${singleIndex} Không phải là một con số hợp lệ`, threadID, messageID);
+        }
+        return api.sendMessage(`𝙏𝙪̛̀ 𝘾𝙝𝙤̂́𝙞 ✅`, threadID, messageID);
+    }
+    else {
+
+        const index = body.split(/\s+/);
+        const fs = require("fs");       
+        for (const singleIndex of index) {
+            if (isNaN(singleIndex) || singleIndex <= 0 || singleIndex > handleReply.pending.length) return api.sendMessage(`${singleIndex} Không phải là một con số hợp lệ`, threadID, messageID);
+            api.unsendMessage(handleReply.messageID);
+            api.changeNickname(`〖 ${global.config.PREFIX} 〗• ${(!global.config.BOTNAME) ? "ditmemay" : global.config.BOTNAME}`, handleReply.pending[singleIndex - 1].threadID, api.getCurrentUserID());
+            api.sendMessage("", event.threadID, () => api.sendMessage({body:`➢ 𝙉𝙝𝙖̣̂𝙣 𝙇𝙚̣̂𝙣𝙝 𝙆𝙞𝙘𝙠 𝙃𝙤𝙖̣𝙩 𝙏𝙪̛̀ 𝘼𝙙𝙢𝙞𝙣\n➢ 𝘾𝙝𝙪́𝙘 𝘾𝙖́𝙘 𝘽𝙖̣𝙣 𝘿𝙪̀𝙣𝙜 𝘽𝙤𝙩 𝙑𝙪𝙞 𝙑𝙚̉`, attachment: fs.createReadStream(__dirname + "/trogiup/menu/trogiup.png")} ,handleReply.pending[singleIndex - 1].threadID));
+            count+=1;
+            
+        }
+        return api.sendMessage(`𝙋𝙝𝙚̂ 𝘿𝙪𝙮𝙚̣̂𝙩 ✅`, threadID, messageID);
+    }
 }
 
-module.exports.run = async function({ api, event, getText }) {
-	const { threadID, messageID } = event;
-		const commandName = this.config.name;
-		var msg = "", index = 1;
+module.exports.run = async function({ api, event, args, permission, handleReply }) {
+        if (args.join() == "") {api.sendMessage("Bạn có thể dùng pending:\nPending user: Hàng chờ người dùng\nPending thread: Hàng chờ nhóm\nPending all:Tất cả hàng chờ ",event.threadID, event.messageID);
+    }
+        const content = args.slice(1, args.length);   
+     switch (args[0]) {
+    case "user":
+    case "u":
+    case "-u":
+    case "User": {
+    const { threadID, messageID } = event;
+    const commandName = this.config.name;
+    var msg = "", index = 1;
+    
+    try {
+        var spam = await api.getThreadList(100, null, ["OTHER"]) || [];
+        var pending = await api.getThreadList(100, null, ["PENDING"]) || [];
+    } catch (e) { return api.sendMessage("𝙇𝙤̂̃𝙞 🚫", threadID, messageID) }
 
-		try {
-		var spam = await api.getThreadList(100, null, ["OTHER"]) || [];
-		var pending = await api.getThreadList(100, null, ["PENDING"]) || [];
-	} catch (e) { return api.sendMessage(getText("cantGetPendingList"), threadID, messageID) }
+      const list = [...spam, ...pending].filter(group => group.isGroup == false);
 
-	const list = [...spam, ...pending].filter(group => group.isSubscribed && group.isGroup);
+    for (const single of list) msg += `${index++}/ ${single.name}(${single.threadID})\n`;
 
-		for (const single of list) msg += `${index++}/ ${single.name}(${single.threadID})\n`;
+    if (list.length != 0) return api.sendMessage(`𝘿𝙖𝙣𝙝 𝙎𝙖́𝙘𝙝 𝘾𝙖̂̀𝙣 𝘿𝙪𝙮𝙚̣̂𝙩 : ${list.length} 𝙉𝙜𝙪̛𝙤̛̀𝙞 𝘿𝙪̀𝙣𝙜\n\n${msg}`, threadID, (error, info) => {
+        global.client.handleReply.push({
+            name: commandName,
+            messageID: info.messageID,
+            author: event.senderID,
+            pending: list
+        })
+    }, messageID);
+    else return api.sendMessage("𝙏𝙧𝙤̂́𝙣𝙜 🛡️", threadID, messageID);
+}
+    case "thread":
+    case "-t":
+    case "t":
+    case "Thread": {
+     const { threadID, messageID } = event;
+    const commandName = this.config.name;
+    var msg = "", index = 1;
+    
+    try {
+        var spam = await api.getThreadList(100, null, ["OTHER"]) || [];
+        var pending = await api.getThreadList(100, null, ["PENDING"]) || [];
+    } catch (e) { return api.sendMessage("𝙇𝙤̂̃𝙞 🚫", threadID, messageID) }
 
-		if (list.length != 0) return api.sendMessage(getText("returnListPending", list.length, msg), threadID, (error, info) => {
-		global.client.handleReply.push({
-						name: commandName,
-						messageID: info.messageID,
-						author: event.senderID,
-						pending: list
-				})
-	}, messageID);
-		else return api.sendMessage(getText("returnListClean"), threadID, messageID);
+    const list = [...spam, ...pending].filter(group => group.isSubscribed && group.isGroup);
+
+    for (const single of list) msg += `${index++}/ ${single.name}(${single.threadID})\n`;
+
+    if (list.length != 0) return api.sendMessage(`𝘿𝙖𝙣𝙝 𝙎𝙖́𝙘𝙝 𝘾𝙖̂̀𝙣 𝘿𝙪𝙮𝙚̣̂𝙩 : ${list.length} 𝙉𝙝𝙤́𝙢\n\n${msg}`, threadID, (error, info) => {
+        global.client.handleReply.push({
+            name: commandName,
+            messageID: info.messageID,
+            author: event.senderID,
+            pending: list
+        })
+    }, messageID);
+    else return api.sendMessage("𝙏𝙧𝙤̂́𝙣𝙜 🛡️", threadID, messageID);
+        }
+    case "all":
+    case "a":
+    case "-a":
+    case "al": {
+     const { threadID, messageID } = event;
+    const commandName = this.config.name;
+    var msg = "", index = 1;
+    
+    try {
+        var spam = await api.getThreadList(100, null, ["OTHER"]) || [];
+        var pending = await api.getThreadList(100, null, ["PENDING"]) || [];
+    } catch (e) { return api.sendMessage(" 𝙇𝙤̂̃𝙞 🚫", threadID, messageID) }
+
+            const listThread = [...spam, ...pending].filter(group => group.isSubscribed && group.isGroup);
+        const listUser = [...spam, ...pending].filter(group => group.isGroup == false)
+    const list = [...spam, ...pending].filter(group => group.isSubscribed);
+
+    for (const single of list) msg += `${index++}/ ${single.name}(${single.threadID})\n`;
+
+    if (list.length != 0) return api.sendMessage(`𝘿𝙖𝙣𝙝 𝙎𝙖́𝙘𝙝 𝘾𝙖̂̀𝙣 𝘿𝙪𝙮𝙚̣̂𝙩 : ${list.length} 𝙐𝙨𝙚𝙧 & 𝙏𝙝𝙧𝙚𝙖𝙙\n\n${msg}`, threadID, (error, info) => {
+        global.client.handleReply.push({
+            name: commandName,
+            messageID: info.messageID,
+            author: event.senderID,
+            pending: list
+        })
+    }, messageID);
+    else return api.sendMessage("𝙏𝙧𝙤̂́𝙣𝙜 🛡️", threadID, messageID);
+        }
+    }       
 }
