59d3d4723bae6e40bf1a50d6118fbddb607a818c
かいと <160225758+AdoboWu@users.noreply.github.com>
2024-03-04 22:52:32 +0800

    Update sendnoti.js

--- script/sendnoti.js
+++ script/sendnoti.js
@@ -5,7 +5,7 @@ const path = require("path");
 module.exports.config = {
 	name: "send",
 	version: "1.1.0",
-	haspermission: 2,
+	hasPermission: 2,
 	description: "Sends a message to all groups and can only be done by the admin.",
 	hasPrefix: false,
 	aliases: ["toni"],
